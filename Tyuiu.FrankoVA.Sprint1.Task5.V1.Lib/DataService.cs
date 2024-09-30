6e1d91bdef93a99b33d0c0b68ef4273fb7021cb5
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-09-30 22:08:16 +0500

    Task5

---
+++ Tyuiu.FrankoVA.Sprint1.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.FrankoVA.Sprint1.Task5.V1.Lib
+{
+    public class DataService :ISprint1Task5V1
+    {
+        public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
+        {
+            var res = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+            int result = Convert.ToInt32(res);
+            return result;
+        }
+    }
+}
+//AB = √(xb - xa)2 + (yb - ya)2
---
+++ Tyuiu.FrankoVA.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.FrankoVA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.FrankoVA.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x1 = 1;
+            int y1 = 4;
+            int x2 = 3;
+            int y2 = 7;
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int result = Convert.ToInt32(res);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+//AB = √(xb - xa)2 + (yb - ya)2
